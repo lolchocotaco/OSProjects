29ade660dc37051964bee7c1ae26e3a7566ff1b5
Sameer <schauhan19@gmail.com>
2013-10-05 01:24:31 -0400

    Basic 'ls' look accomplished

--- find/find.c
+++ find/find.c
@@ -15,7 +15,54 @@
 #include <sys/stat.h>      /* defines S_IREAD & S_IWRITE  */
 #include <dirent.h>
 #include <limits.h> 		/*defints PATHMAX */
+#include <pwd.h>
+#include <time.h>
+#include <grp.h>
 extern int errno;
+
+
+void detailedInfo(char * dirName){
+		struct stat fileStat;
+
+	if(stat(dirName,&fileStat)>=0){
+		struct passwd * ownerInfo;
+		struct group * groupInfo;
+		char mTime[20];
+
+		//Devide and iNode Numbre
+		printf("%ld/%lu\t",fileStat.st_dev,fileStat.st_ino);
+		// Permissions
+		printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
+	    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
+	    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
+	    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
+	    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
+	    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
+	    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
+	    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
+	    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
+	    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
+	    //Number of links
+	   	printf("\t%ld",fileStat.st_nlink);
+	   	// Owner of file
+	   	if( (ownerInfo = getpwuid(fileStat.st_uid) )!= NULL)
+	   		printf("\t%s",ownerInfo->pw_name);
+	   	else
+	   		printf("\t%d",fileStat.st_uid);
+	   	// Group of file
+	   	if( (groupInfo = getgrgid(fileStat.st_gid) ) !=NULL)
+	   		printf("\t%s",groupInfo->gr_name);
+	   	else
+	   		printf("\t%ld",(long)fileStat.st_gid);
+	   	// Size of file
+	   	printf("\t%ld", fileStat.st_size);
+	   	// Last Modified
+	   	strftime(mTime,20,"%b %d %Y %H:%M",localtime(&fileStat.st_mtime));
+	   	printf("\t%s",mTime);
+	   	//File Name
+	   	printf("\t%s\n",dirName);
+	}
+}
 
 
 void listFiles(char* dirName){
@@ -27,17 +74,22 @@ void listFiles(char* dirName){
 	if(dp != NULL){
 		while(sp = readdir(dp)){
 			d_name = sp->d_name;
-			printf("%s/%s\n",dirName,sp->d_name);
-			if(sp->d_type == DT_DIR && strcmp(sp->d_name, "..") != 0 && strcmp(sp->d_name,".") != 0){
+			if(strcmp(sp->d_name, "..") != 0 && strcmp(sp->d_name,".") != 0){
 				int path_length;
 				char path[PATH_MAX];
 				path_length = snprintf(path, PATH_MAX,"%s/%s",dirName,d_name);
-				if(path_length >= PATH_MAX){
-					fprintf(stderr,"Path Legnth is too long. \n");
-					exit(1);
+				detailedInfo(path);
+				// printf("%s\n",path);
+				// printf("%s/%s\n",dirName,sp->d_name);
+				if(sp->d_type == DT_DIR){
+
+					if(path_length >= PATH_MAX){
+						fprintf(stderr,"Path Length is too long. \n");
+						exit(1);
+					}
+					listFiles(path);
 				}
-				listFiles(path);
-			}
+			}			
 		}
 		(void)closedir(dp);
 	}else{
@@ -67,7 +119,7 @@ int main (int argc, char **argv){
 	   break;
 	 case 'm':
 	   mflag = 1;
-	   // bufferSize = atoi(optarg); // Save the buffer size
+	   // mTimeerSize = atoi(optarg); // Save the mTimeer size
 	   break;
 	 case '?':
 	   if (optopt == 'u' || optopt =='m')
@@ -89,6 +141,8 @@ int main (int argc, char **argv){
 	}
 
 	listFiles(dirName);
+
+
 
 	return 0;
 }
