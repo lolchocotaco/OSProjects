ba6ce3c5c44ce9d7263048783144793cb353bf01
Sameer <schauhan19@gmail.com>
2013-10-04 23:30:46 -0400

    Some recursive directory listing done

--- find/find.c
+++ find/find.c
@@ -14,20 +14,48 @@
 #include <sys/types.h>     /* defines types used by sys/stat.h */
 #include <sys/stat.h>      /* defines S_IREAD & S_IWRITE  */
 #include <dirent.h>
-
+#include <limits.h> 		/*defints PATHMAX */
 extern int errno;
+
+
+void listFiles(char* dirName){
+	DIR * dp; //Directory Pointer
+	struct dirent *sp; //Directory Structure pointer
+	char * d_name;
+
+	dp = opendir(dirName);
+	if(dp != NULL){
+		while(sp = readdir(dp)){
+			d_name = sp->d_name;
+			printf("%s/%s\n",dirName,sp->d_name);
+			if(sp->d_type == DT_DIR && strcmp(sp->d_name, "..") != 0 && strcmp(sp->d_name,".") != 0){
+				int path_length;
+				char path[PATH_MAX];
+				path_length = snprintf(path, PATH_MAX,"%s/%s",dirName,d_name);
+				if(path_length >= PATH_MAX){
+					fprintf(stderr,"Path Legnth is too long. \n");
+					exit(1);
+				}
+				listFiles(path);
+			}
+		}
+		(void)closedir(dp);
+	}else{
+		fprintf(stderr,"Could not open directory '%s': %s\n", dirName, strerror(errno));
+		exit(1);
+	}
+
+}
 
 
 int main (int argc, char **argv){
 	int index;
 	int optVal;
-
 	int uflag = 0;
 	int mflag = 0;
 	opterr = 0;
-	DIR * dp; //Directory Pointer
-	struct dirent *sp; //Directory Structure pointer
 	char * dirName;
+	
 
 	// Find the operators
 	while ((optVal = getopt (argc, argv, "u:m:")) != -1) {
@@ -55,20 +83,12 @@ int main (int argc, char **argv){
 
 
 	if(optind == argc){
-		dirName = "./";
+		dirName = ".";
 	} else{
 		dirName = argv[optind];
 	}
 
-	dp = opendir(dirName);
-	if(dp != NULL){
-		while(sp = readdir(dp)){
-			puts(sp->d_name);
-		}
-		(void)closedir(dp);
-	}else{
-		fprintf(stderr,"Could not open directory '%s': %s\n", dirName, strerror(errno));
-	}
+	listFiles(dirName);
 
 	return 0;
 }
