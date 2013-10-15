0bf4deae565c7e1287a0748197633dc38e65c993
Sameer <schauhan19@gmail.com>
2013-10-15 19:35:38 -0400

    started shell

---
+++ shell/shell.c
@@ -0,0 +1,26 @@
+////////////////////
+//Sameer Chauhan  //
+//ECE357          //
+//Find Program //
+////////////////////  
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>         /* defines options flags */
+#include <sys/types.h>     /* defines types used by sys/stat.h */
+#include <sys/stat.h>      /* defines S_IREAD & S_IWRITE  */
+#include <dirent.h>
+#include <limits.h> 		/*defines PATH_MAX */
+#include <pwd.h>
+#include <time.h>	
+#include <grp.h>
+extern int errno;
+
+int main(int argc, char **argv){
+	printf("Helloworld\n");
+
+}
