6751e9428d3fff18917a9f466a2d88001d3e580e
Sameer <schauhan19@gmail.com>
2013-10-16 11:50:28 -0400

    started shell

--- shell/shell.c
+++ shell/shell.c
@@ -20,7 +20,53 @@
 #include <grp.h>
 extern int errno;
 
-int main(int argc, char **argv){
-	printf("Helloworld\n");
+#define MAX_LENGTH 1024
+#define DELIMS " \t\r\n"
 
+int main(int argc, char *argv[]) {
+  char *cmd;
+  char line[MAX_LENGTH];
+  int pid;
+  int status;
+
+  while (1) {
+    printf("$ ");
+    if (!fgets(line, MAX_LENGTH, stdin)) break;
+
+    // Parse and execute command
+    if ((cmd = strtok(line, DELIMS))) {
+      // Clear errors
+      errno = 0;
+
+      if (strcmp(cmd, "cd") == 0) {
+        char *arg = strtok(0, DELIMS);
+
+        if (!arg) fprintf(stderr, "cd missing argument.\n");
+        else chdir(arg);
+
+      } else if (strcmp(cmd, "exit") == 0) {
+        break;
+      } else {
+      	printf("You want to run command %s\n", line);
+      	switch(pid = fork()){
+      		case -1:
+      			perror("Failed to create fork: ");
+      			exit(1);
+      		case 0:
+      			if(execvp(argv[0],&argv[0]) <0){
+      				perror("Failed to exec: ");
+      				exit(1);
+      			}
+      		default:
+      			wait(&status);
+      			printf("I'm the parent\n");
+      	}
+      }
+      
+
+      if (errno) perror("Command failed");
+    }
+  }
+
+  return 0;
 }
