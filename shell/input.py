486939e0008798dbe8688abaf061c6522845c21a
Sameer <schauhan19@gmail.com>
2013-10-18 20:48:54 -0400

    Testing redirection

---
+++ shell/input.py
@@ -0,0 +1,4 @@
+#!/user/bin/python
+
+str = raw_input("Print stuff: ")
+print("You typed: {0}").format(str)
--- shell/shell.c
+++ shell/shell.c
@@ -25,120 +25,163 @@
 
 #define MAX_LENGTH 1024
 #define DELIMS " \t\r\n"
+#define DELIMS2 " \t\r\n<>"
+
+void redirect(char *fName, int stdFD, int options	){
+	int fd;
+	char* fileName  = strtok(fName,"<>");
+	// printf("Redirect to: %s\n",fileName);
+	if( (fd = open(fileName,options,S_IREAD | S_IWRITE ) ) <0 ){
+		fprintf(stderr, "Cannot open file %s: %s\n",fileName, strerror(errno));
+		exit(1);
+	}
+	if(dup2(fd,stdFD)<0){
+		fprintf(stderr,"Cannot dup %s to %d: %s",fileName,stdFD,strerror(errno));
+	}
+	close(fd);
+}
 
 
 
 int main(int argc, char *argv[]) {
-  char *cmd;
-  char *cmdArg;
-  char line[MAX_LENGTH];
-  int pid, status, count;
-  char *cmdList[MAX_LENGTH];
-  int ofd,ifd;
-  char *fileName;
-  struct rusage ru;
-  pid_t cpid;
-  int  cstart, cend;
-  char *split;
-  FILE* inFile = NULL;
-  FILE* input = NULL;
+	char *cmd;
+	char *cmdArg;
+	char line[MAX_LENGTH];
+	int pid, status, count;
+	char *cmdList[MAX_LENGTH];
+	
+	struct rusage ru;
+	int  cstart, cend;
+	FILE* input = NULL;
+
+	char *fName;
+	int stdFD;
+	int options;
+	int isRedirect = 0;
 
 
 //If there is an argument, open up that file for reading
-if(argc> 1){
-	if((inFile = fopen(argv[1],"r") ) != NULL){
-		input = inFile;
-	}	
-	else{
-		perror("Open failed ");
-		exit(1);
+	if(argc> 1){
+		if((input = fopen(argv[1],"r") ) == NULL){
+			perror("Open failed ");
+			exit(1);
+		}
+	} else{
+		input = stdin;
 	}
-} else{
-	input = stdin;
-}
 
-// line = line of input
-// cmd = name of cmd 
-// cmdArg = arguments of cmd
-// cmdList = array to passed into Exec (reset everytime)
-printf("mysh> ");
-  while (1) {
+	// line = line of input
+	// cmd = name of cmd 
+	// cmdArg = arguments of cmd
+	// cmdList = array to passed into Exec (reset everytime)
+	printf("mysh> ");
+	while (1) {
+		//Reset at each loop though
+		errno = 0;
+		isRedirect = 0;
+		//Read input (if there is nothing, break to end program)
+		if (!fgets(line, MAX_LENGTH, input)) {
+			printf("\n");
+			break;
+		}
+		//If there is a comment move on. 
+		if(line[0] == '#'){
+			continue;
+		}
 
-    //Read input (if there is nothing, break to end program)
-    if (!fgets(line, MAX_LENGTH, input)) {
-    	printf("\n");
-    	break;
-    }
-    //If there is a comment move on. 
-    if(line[0] == '#'){
-    	continue;
-    }
-  	//Clear cmdList at each command
-  	memset(&cmdList[0], 0, sizeof(cmdList));
+		//Clear cmdList at each command
+		memset(&cmdList[0], 0, sizeof(cmdList));
 
-    // Get parameter stuff
-    if ((cmd = strtok(line, DELIMS))) {
-      errno = 0;
-      cmdList[0] = cmd;
+		// Get parameter stuff
+		char *p = line;
+		if ((cmd = strtok_r(p, DELIMS,&p))) {
+		  errno = 0;
+		  cmdList[0] = cmd;
 
+			printf("Executing command %s with arguments", cmd);
+			count = 1;
+			while(cmdArg = strtok_r(0,DELIMS,&p)){
+				// Check if argument is for redirection
+				if(cmdArg[0] == '<'){
+					isRedirect = 1;
+					fName = cmdArg;
+					stdFD = 0;
+					options = O_RDONLY;
+					// redirect(cmdArg,0, O_RDONLY);
+					// printf("\n\n<Dups: %s\n",strtok(cmdArg,"<>"));
+				}
+				else if(cmdArg[0] == '>'){
+					isRedirect = 1;
+					fName = cmdArg;
+					stdFD =  1;
+					if(cmdArg[1]=='>'){
+						options = O_CREAT|O_APPEND|O_WRONLY;
+					} else{
+						options = O_CREAT|O_TRUNC|O_WRONLY;
+					}
+					// printf("\n\n>Dups: %s\n",strtok(cmdArg,"<>"));
+				}
+				else if(cmdArg[0] == '2'){
+					isRedirect = 1;
+					//Some character stripping  (useless apce)
+					char* two = strtok(cmdArg,"<>"); 
+					fName = strtok(0,"<>");
+					stdFD = 2;
+					if(cmdArg[2] == '>'){
+						options = O_CREAT|O_APPEND|O_WRONLY;
+						// printf("dubs");
+					} else{
+						options = O_CREAT|O_TRUNC|O_WRONLY;
+					}
+				}
+				else{
+			  		printf(" '%s'",cmdArg);
+			  		cmdList[count] = cmdArg;
+			  		count++;
+				}
+				
+			}
+			printf("\n");
+			cmdList[count] = NULL;
 
-      // First condition is if cd is called. Make it work with exec
-      if (strcmp(cmd, "cd1") == 0) {
-        cmdArg = strtok(0, DELIMS);
+			switch(pid = fork()){
+				case -1:
+					perror("Failed to create fork ");
+					exit(1);
+					break; // Not reached
+				case 0: //Child process
+					cstart = clock();
+					if(isRedirect){
+						redirect(fName,stdFD,options);
+					}
 
-        if (!cmdArg) 
-        	fprintf(stderr, "cd missing argument.\n");
-        else 
-        	chdir(cmdArg);
-      } 
-      else if (strcmp(cmd, "exit") == 0) {
-        break;
-      } 
-      ///CD Nonsense
-      else {
-      	printf("Executing command %s with arguments", cmd);
-      	count = 1;
-      	while(cmdArg = strtok(0,DELIMS)){
-      		printf(" '%s'",cmdArg);
-      		cmdList[count] = cmdArg;
-      		count++;
-      	}
-      	printf("\n");
-      	cmdList[count] = NULL;
-      	switch(pid = fork()){
-      		case -1:
-      			perror("Failed to create fork ");
-      			exit(1);
-      			break; // Not reached
-      		case 0: //Child process
-      			cstart = clock();
-      			if(execvp(cmdList[0],cmdList) <0){
-      				perror("Failed to exec ");
-      				exit(-1);
-      			}
-      			cend = clock();
-      			break;
-      		default: //Parent process
-      			if(wait3(&status,0,&ru)== -1)
-      				perror("Wait failed ");
-      			else{
-      				fprintf(stderr,"Command returned with return code %d,\n",status);
-      				fprintf(stderr,"consuming %6f real seconds %ld.%6d user, %ld.%6d system \n", 
-      					(double)(cend-cstart),
-      					ru.ru_utime.tv_sec, 
-      					(int)ru.ru_utime.tv_usec,
-      					ru.ru_stime.tv_sec,
-      					(int)ru.ru_stime.tv_usec);
-      			}
-      			break;
-      		}
-      }
-      
-      if (errno) 
-      	perror("Command failed ");
-    }
-    printf("mysh> ");
-  }
+					if(execvp(cmdList[0],cmdList) <0){
+						perror("Failed to exec ");
+						exit(-1);
+					}
+					cend = clock();
+					break;
+				default: //Parent process
+					if(wait3(&status,0,&ru)== -1)
+						perror("Wait failed ");
+					else{
+						fprintf(stderr,"Command returned with return code %d,\n",status);
+						fprintf(stderr,"consuming %6f real seconds %ld.%6d user, %ld.%6d system \n", 
+							(double)(cend-cstart),
+							ru.ru_utime.tv_sec, 
+							(int)ru.ru_utime.tv_usec,
+							ru.ru_stime.tv_sec,
+							(int)ru.ru_stime.tv_usec);
+					}
+					break;
+				}
+		  
+		  
+		  if (errno) 
+		  	perror("Command failed ");
+		}
+		printf("mysh> ");
+	}
 
   return 0;
 }
