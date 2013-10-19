5daa3ae418c3d7c5234dc67515d9768ec67518a2
Sameer <schauhan19@gmail.com>
2013-10-19 19:16:32 -0400

    Basic functionality implemented. Need to keep track of bytes written

--- catgrepmore/catgrep.c
+++ catgrepmore/catgrep.c
@@ -10,20 +10,21 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#include <fcntl.h>         /* defines options flags */
-#include <sys/types.h>     /* defines types used by sys/stat.h */
-#include <sys/stat.h>      /* defines S_IREAD & S_IWRITE  */
+#include <sys/stat.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
 
-extern int errno;
+#define BUFFER_SIZE 16384
 
+int numFiles = 0;
 
 void int_handler(int sig){
-	fprintf(stderr,"Signal received: %d\n",sig);
-	exit(2);
+	fprintf(stderr,"\n\nProcess interrupted\n");
+	fprintf(stderr,"Files processed: %d\n",numFiles);
+	exit(-1);
 }
-
-
+	
 int main (int argc, char **argv){
 
 	if(argc<3){
@@ -31,49 +32,51 @@ int main (int argc, char **argv){
 		exit(-1);
 	}
 
-	int index;
+	// Handling interrupts
+	signal(SIGINT,int_handler);
 
-	int grepPid,morePid;
+	int index,grepPid,morePid,status;
+
 
 
 	//File Descriptors
 	int inFile;
 	int fds[2];
 	char* grepCmd[4] = {"grep",argv[1],NULL,NULL};
-	printf("inputfiles: \n");
 
-	if (pipe(fds)<0){
-		perror("Can't create pipe");
-		return -1;
-	}
+				
 
 	for(index = 2; index<argc; index++){
-		printf("Grepping %s with pattern: %s\n",argv[index],argv[1]);
+		printf("Grepping %d out of %d %s with pattern: %s\n",index+1,argc,argv[index],argv[1]);
 		grepCmd[2] = argv[index];
 		// if( (inFile = open(argv[index], O_RDONLY, 0666))<0){
 		// 	fprintf(stderr,"Error occured while opening %s: %s\n", argv[index], strerror(errno));
 		// 	return -1;
 		// }
+		if (pipe(fds)<0){
+			perror("Can't create pipe");
+			return -1;
+		}	
+	
 		switch((grepPid = fork())){
 			case -1:
 				perror("Failed to create fork ");
 				exit(-1);
 				break; 
 			case 0: //Child process
-				printf("in the child\n");
+				signal(SIGINT,int_handler);
 				close(fds[0]); //Close unused read end;
 				if(dup2(fds[1],1)<0){
-					fprintf(stderr,"Cannot dup stdout to pipe fds[1]",strerror(errno));
+					fprintf(stderr,"Cannot dup stdout to pipe fds[1]\n",strerror(errno));
 					exit(-1);
 				}
-				close(fds[1]);
 				execvp(grepCmd[0],grepCmd);
-				
 				break;
 			default: //Parent process
-				printf("In the parent of of grep\n");
+				waitpid(grepPid,&status,WUNTRACED);
+				close(fds[1]);
+				numFiles++;
 				break;
-
 		}
 		
 		switch ((morePid = fork())){
@@ -82,73 +85,18 @@ int main (int argc, char **argv){
 				exit(-1);
 				break; 
 			case 0: //Child process
-				signal(SIGINT,int_handler);
-				printf("in the child\n");
 				close(fds[1]); // Close unused write end. 
 				if(dup2(fds[0],0)<0) {
-					fprintf(stderr,"Cannot dup stdin to pipe fds[0]",strerror(errno));
+					fprintf(stderr,"Cannot dup stdin to pipe fds[0]\n",strerror(errno));
 					exit(-1);
 				}
-				close(fds[0]); 
-				execlp("pg","pg",NULL);
-
-				//tell more to read fd[0]
+				execlp("more","more",NULL);
 				break;
 			default: //Parent process
-				printf("In the parent of more\n");
+				waitpid(morePid,&status,0);
+				close(fds[0]);
 				break;
 		}
 	}
-
-
-/*
-	Loads the Output file. If none is specified, stdout is chosen;
-//  */
-// if(oflag ==1  && outName !=NULL){
-//   if (  (outFile = open(outName, O_WRONLY |O_CREAT | O_TRUNC, 0666 ) )== -1){
-//     fprintf(stderr,"Error occured while opening %s: %s\n", outName, strerror(errno));
-//     return -1;
-//   }
-// }
-// else{
-//   outFile = 1;
-// }
-
-/* Allocates proper memory */
-// bufferPtr = malloc(bufferSize);
-// if(bufferPtr == NULL){
-//   fprintf(stderr,"Cannot allocate buffer of size %d\n",bufferSize);
-//   return -1;
-// }
-/*
-	If no options are supplied the input is stdin.
-	goes through remaining arguments and applyies copycat function. 
-
- */
-// else{
-//   for(index = optind; index <argc; index++){
-//     if( argv[index][0] == '-')  //getOpt handles any other (-foo) arguments
-//       inFile = 0;
-//     else if( (inFile = open(argv[index], O_RDONLY, 0666))== -1){
-//       fprintf(stderr,"Error occured while opening %s: %s\n", argv[index], strerror(errno));
-//       return -1;
-//     }
-		 
-//     switch (copycat(inFile,outFile)){
-//       case 0:
-//         break;
-//       case 1:
-//         fprintf(stderr,"Error occured while opening %s: %s\n", argv[index], strerror(errno));
-//         return -1;
-//       case 2:
-//         fprintf(stderr,"Error occured while opening %s: %s\n", outName, strerror(errno));
-//         return -1;
-//       default:
-//         break;
-//     }
-
-//   }
-// }
-
  return 0;
 }
